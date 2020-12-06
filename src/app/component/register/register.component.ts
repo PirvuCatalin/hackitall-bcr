7f44cca89a9ee4dd981a759607932276baa52ed6
PirvuCatalin <gabriel.pirvu0911@stud.acs.upb.ro>
2020-12-06 08:20:49 +0200

    added camera upload feature

--- src/app/component/register/register.component.ts
+++ src/app/component/register/register.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { ModalDirective } from 'angular-bootstrap-md';
 import { RegisterService } from 'src/app/service/register.service';
 
 @Component({
@@ -8,7 +9,19 @@ import { RegisterService } from 'src/app/service/register.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  constructor(private registerService: RegisterService, private router: Router) { }
+  @ViewChild('demoBasic', { static: true }) modal: ModalDirective | undefined;
+  @ViewChild('video', { static: true }) videoElement: ElementRef | undefined;
+  @ViewChild('canvas', { static: true }) canvas: ElementRef | undefined;
+
+  constructor(private registerService: RegisterService, private router: Router, private renderer: Renderer2) { }
+
+  constraints = {
+    video: {
+      facingMode: "environment",
+      width: { ideal: 4096 },
+      height: { ideal: 2160 }
+    }
+  };
 
   uploadIdHidden = true;
   checkButtonHidden = true;
@@ -20,6 +33,9 @@ export class RegisterComponent implements OnInit {
 
   passwordOk = false;
   phoneOk = false;
+
+  showSelfieCamera = true;
+  showIdCamera = false;
 
   imageFile: any = null;
   idFile: any = null;
@@ -63,23 +79,108 @@ export class RegisterComponent implements OnInit {
       this.inputDisabled = true;
       this.imageFile = file.files[0];
       this.uploadIdHidden = false;
+      this.showIdCamera = true;
+      this.showSelfieCamera = false;
     }
 
   }
 
   handleIDInput(file: any) {
     this.checkButtonHidden = false;
+    this.showIdCamera = false;
     this.idFile = file.files[0];
+  }
+
+  cameras: any = null;
+
+  startCamera() {
+    if (!!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)) {
+      navigator.mediaDevices.getUserMedia(this.constraints).then(this.attachVideo.bind(this)).catch(this.handleError);
+    } else {
+      alert('Sorry, camera not available.');
+    }
+  }
+
+  handleError(error: any) {
+    console.log('Error: ', error);
+  }
+
+  videoWidth = 0;
+  videoHeight = 0;
+
+  srcToFile(src: any, fileName: any, mimeType: any) {
+    return (fetch(src)
+      .then(function (res) { return res.arrayBuffer(); })
+      .then(function (buf) { return new File([buf], fileName, { type: mimeType }); })
+    );
+  }
+
+  capture() {
+    this.renderer.setProperty(this.canvas?.nativeElement, 'width', this.videoWidth);
+    this.renderer.setProperty(this.canvas?.nativeElement, 'height', this.videoHeight);
+    this.canvas?.nativeElement.getContext('2d').drawImage(this.videoElement?.nativeElement, 0, 0);
+
+    var canvas = <HTMLCanvasElement>document.getElementById("canvas");
+
+    let that = this;
+    this.srcToFile(canvas?.toDataURL("image/png"), "testName", "image/png")
+      .then(function (file) {
+        if (that.showSelfieCamera) {
+          that.modal?.hide();
+    
+          that.cameras.getTracks().forEach(function (track: any) {
+            track.stop();
+          });
+    
+          if (!that.checkIfEmail()) {
+            document.getElementById("defaultRegisterFormEmail")?.classList.add("is-invalid");
+          } else {
+            that.inputDisabled = true;
+            that.imageFile = file;
+            that.uploadIdHidden = false;
+          }
+    
+          that.showSelfieCamera = false;
+          that.showIdCamera = true;
+        } else {
+          that.modal?.hide();
+    
+          that.cameras.getTracks().forEach(function (track: any) {
+            track.stop();
+          });
+    
+          that.checkButtonHidden = false;
+          that.idFile = file;
+          that.showIdCamera = false;
+        }
+      });
+  }
+
+  attachVideo(stream: any) {
+    this.cameras = stream;
+    this.renderer.setProperty(this.videoElement?.nativeElement, 'srcObject', stream);
+    this.renderer.listen(this.videoElement?.nativeElement, 'play', (event) => {
+      this.videoHeight = this.videoElement?.nativeElement.videoHeight;
+      this.videoWidth = this.videoElement?.nativeElement.videoWidth;
+    });
+  }
+
+  openSelfieCamera() {
+    if (this.modal) {
+      this.modal.show();
+
+      this.startCamera();
+    }
   }
 
   checkImages() {
     var bypassChecks = (<HTMLInputElement>document.getElementById("bypassChecks")).checked;
-    
+
     console.log(this.imageFile);
     console.log(this.idFile);
     this.spinnerHidden = false;
 
-    if(bypassChecks) {
+    if (bypassChecks) {
       this.registerService.bypassUploadImages(this.email, this.imageFile, this.idFile).subscribe(data => {
         this.spinnerHidden = true;
         if (data["errors"] && data["errors"].length > 0) {
@@ -95,6 +196,7 @@ export class RegisterComponent implements OnInit {
           this.uploadIdHidden = true;
           this.checkButtonHidden = true;
           this.retakePhotosAlertHidden = false;
+          this.showSelfieCamera = true;
         } else {
           // do something, if upload success
           console.log("success");
@@ -108,15 +210,16 @@ export class RegisterComponent implements OnInit {
             signupScreen.style.display = "block";
           }
         }
-  
-  
+
+
       }, error => {
         console.log("error");
         console.log(error);
-  
+
         this.uploadIdHidden = true;
         this.checkButtonHidden = true;
         this.retakePhotosAlertHidden = false;
+        this.showSelfieCamera = true;
       });
     } else {
       this.registerService.uploadImages(this.email, this.imageFile, this.idFile).subscribe(data => {
@@ -134,6 +237,7 @@ export class RegisterComponent implements OnInit {
           this.uploadIdHidden = true;
           this.checkButtonHidden = true;
           this.retakePhotosAlertHidden = false;
+          this.showSelfieCamera = true;
         } else {
           // do something, if upload success
           console.log("success");
@@ -147,19 +251,20 @@ export class RegisterComponent implements OnInit {
             signupScreen.style.display = "block";
           }
         }
-  
-  
+
+
       }, error => {
         console.log("error");
         console.log(error);
-  
+
         this.uploadIdHidden = true;
         this.checkButtonHidden = true;
         this.retakePhotosAlertHidden = false;
+        this.showSelfieCamera = true;
       });
     }
 
-    
+
   }
 
   onKeyPassword(event: any) { // without type info
